e0ee05bb9faec6a58944279201592856f591883c
jsturm-11 <114809861+jsturm-11@users.noreply.github.com>
2022-10-01 13:13:57 +0200

    Add files via upload

---
+++ SendLinkviaOutlook.ps1
@@ -0,0 +1,12 @@
+﻿#File has to be in %appdata%\Microsoft\Windows\SendTo
+
+
+param($p1)
+$Outlook = New-Object -ComObject Outlook.Application
+
+$Mail = $Outlook.CreateItem(0)
+$Mail.Subject = ""
+
+$Mail.HTMLBody = "<HTML><BODY>Hier der Link zum Dokument: <a href=""" + $PSBoundParameters["p1"] + """>" + $PSBoundParameters["p1"] + "</a>.</BODY></HTML>"
+
+$Mail.Display(0)
